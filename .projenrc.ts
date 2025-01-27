65d9feed142847b48e3b4be28171f3bf1a956ee3
Ben Moses <benmoses@webfront.co.uk>
2025-01-27 23:34:28 +0000

    feat: did local projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,18 +1,18 @@
-import { typescript } from "projen";
+import { typescript } from 'projen';
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: "main",
-  name: "actions-testing",
-  repository: "https://github.com/benjymoses/actions-testing.git/",
+  defaultReleaseBranch: 'main',
+  name: 'actions-testing',
+  repository: 'https://github.com/benjymoses/actions-testing.git/',
   projenrcTs: true,
   description:
-    "This project is to learn and understand how Projen projects use Github actions to publish packages to NPM.",
+    'This project is to learn and understand how Projen projects use Github actions to publish packages to NPM.',
   // deps: [],                /* Runtime dependencies of this module. */
   // devDeps: [],             /* Build dependencies for this module. */
-  packageName: "actions-testing",
+  packageName: 'actions-testing',
   buildWorkflowOptions: {
     workflowTriggers: {
       push: {
-        branches: ["main"],
+        branches: ['main'],
       },
     },
   },
