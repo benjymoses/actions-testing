b749175b38aabca5716d5cd6ab12138dac113838
Ben Moses <benmoses@webfront.co.uk>
2025-01-28 00:44:41 +0000

    fix(something) This is the commit message \n And this is some more text that may go in to the changelog... \n \n and this is too.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export class Hello {
   public sayHello() {
     const bob = 'bill';
-    console.log(bob);
+    console.log(`Variable "bob" is set to ${bob}`);
     return 'hello, world, does this bump patch version?';
   }
 }
